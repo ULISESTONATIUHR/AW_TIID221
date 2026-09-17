74ea947b70577f4a3f02cdb846916349bd8bff23
ULISESTONATIUHR <125053572@upq.edu.mx>
2026-09-17 15:41:10 -0600

    Prueba

---
+++ index.php
@@ -0,0 +1,30 @@
+<!--?php
+echo   "Hello World";
+?>-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: blue;
+        }
+        
+    </style>
+</head>
+<body>
+    <h1>Hello  Crayola</h1>
+    <h2>Hello  Crayola</h2>
+    <h3>Hello  Crayola</h3>
+    <h4>Hello  Crayola</h4>
+    <h5>Hello  Crayola</h5>
+    <h6>Hello  Crayola</h6>
+
+
+
+
+
+</body>
+</html>
